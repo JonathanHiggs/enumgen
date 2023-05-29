8a0403c568299a1bc0f2362f31b6ce08642c16ea
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2023-05-29 21:44:21 +0100

    Custom cmake config file that includes targets and functions

---
+++ cmake/enumgen-config.cmake
@@ -0,0 +1,10 @@
+# enumgen
+#
+# Defines targets:
+#     enumgen::enumgen
+#
+# The following functions are defined to help with code generation:
+#     enumgen_generate
+
+include("${CMAKE_CURRENT_LIST_DIR}/enumgen-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/enumgen-functions.cmake")
--- cmake/enumgen-functions.cmake
+++ cmake/enumgen-functions.cmake
@@ -1,10 +1,6 @@
 # EnumGen CMake module
 
 
-# Find the enumgen package
-find_package(enumgen REQUIRED)
-
-
 # Generate enum code files for given input and config files
 function(enumgen_generate input_file config_file)
     get_target_property(ENUMGEN_EXE enumgen::enumgen LOCATION)
